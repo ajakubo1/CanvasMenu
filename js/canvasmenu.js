d52478d943220e21fc6ea1252db2b0a809b4a173
Adam Jakubowski <ajakubo1@gmail.com>
2015-10-17 11:17:03 +0200

    Added menu animation functions

--- js/canvasmenu.js
+++ js/canvasmenu.js
@@ -1,101 +1,167 @@
 
-function Menu(canvas, width, height) {
+function Menu(canvas, width, height, animated) {
 	var self = this;
-	this.buttons = new Array(),
-	this.canvas = canvas,
+	this.buttons = [];
+	this.canvas = canvas;
+	this.width = width;
+	this.height = height;
 	this.canvas.width = width;
 	this.canvas.height = height;
 	this.ctx = canvas.getContext('2d');
 	this.scaleX = 1;
 	this.scaleY = 1;
 	this.focused = undefined;
-	
+	this.animated = animated || false;
+	this.running = false;
+	this.updateTime = undefined;
+	this.tickLength = 16.66666666;
+	this.tickCount = 0;
+	this.tickMax = 60;
+
 	this.listener_mousedown = function (event) {
 		var i, x = event.clientX * self.scaleX - canvas.offsetLeft, y = event.clientY * self.scaleY - canvas.offsetTop;
 		for (i = 0; i < self.buttons.length; i += 1) {
 			if (self.buttons[i].inRange(x, y)) {
 				self.buttons[i].setState(BUTTON_ENUM.down);
-				self.redrawMenu();
+				self.redrawButtons();
 				break;
 			}
 		}
-	}
-	
+	};
+
 	this.listener_mousemove = function (event) {
 		var i, x = event.clientX * self.scaleX - canvas.offsetLeft, y = event.clientY * self.scaleY - canvas.offsetTop;
-		
+
 		if (self.focused !== undefined) {
 			if (!self.focused.inRange(x, y)) {
 				self.focused.setState(BUTTON_ENUM.inactive);
 				self.focused = undefined;
-				self.redrawMenu();
+				self.redrawButtons();
 			}
 		}
-		
+
 		for (i = 0; i < self.buttons.length; i += 1) {
 			if (self.buttons[i].inRange(x, y)) {
 				self.focused = self.buttons[i];
 				self.buttons[i].setState(BUTTON_ENUM.focused);
-				self.redrawMenu();
-				break;  
+				self.redrawButtons();
+				break;
 			}
 		}
-	}
-	
+	};
+
 	this.listener_mouseup = function (event) {
 		var i, x = event.clientX * self.scaleX - canvas.offsetLeft, y = event.clientY * self.scaleY - canvas.offsetTop;
 		for (i = 0; i < self.buttons.length; i += 1) {
 			if (self.buttons[i].inRange(x, y)) {
 				self.buttons[i].setState(BUTTON_ENUM.up);
-				self.redrawMenu();
+				self.redrawButtons();
 				self.buttons[i].click();
 				break;
 			}
 		}
-	}
-	
-	this.redrawMenu = function () {
+	};
+
+	this.redrawButtons = function () {
 		var i;
-		this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+		if (this.animated) {
+			this.redrawBackground();
+		} else {
+			this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+		}
 		for (i = 0; i < this.buttons.length; i += 1) {
 			this.ctx.drawImage(this.buttons[i].getCanvas(), this.buttons[i].getX(), this.buttons[i].getY());
 		}
-	}
+	};
+
+	this.redrawBackground = function () {
+		if (self.tickCount % 10 === 0) {
+			this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+			self.ctx.fillStyle = 'rgb(' + (100 + self.tickCount % 5) + ', 0, 0)';
+			self.ctx.fillRect(Math.random() * (self.width - 100), Math.random() * (self.height - 100), 100, 100);
+			self.ctx.fillRect(Math.random() * (self.width - 100), Math.random() * (self.height - 100), 100, 100);
+			self.ctx.fillRect(Math.random() * (self.width - 100), Math.random() * (self.height - 100), 100, 100);
+			self.ctx.fillRect(Math.random() * (self.width - 100), Math.random() * (self.height - 100), 100, 100);
+			self.ctx.fillRect(Math.random() * (self.width - 100), Math.random() * (self.height - 100), 100, 100);
+			self.ctx.fillRect(Math.random() * (self.width - 100), Math.random() * (self.height - 100), 100, 100);
+			self.ctx.fillRect(Math.random() * (self.width - 100), Math.random() * (self.height - 100), 100, 100);
+			self.ctx.fillRect(Math.random() * (self.width - 100), Math.random() * (self.height - 100), 100, 100);
+			self.ctx.fillRect(Math.random() * (self.width - 100), Math.random() * (self.height - 100), 100, 100);
+			self.ctx.fillRect(Math.random() * (self.width - 100), Math.random() * (self.height - 100), 100, 100);
+			self.ctx.fillRect(Math.random() * (self.width - 100), Math.random() * (self.height - 100), 100, 100);
+			self.ctx.fillRect(Math.random() * (self.width - 100), Math.random() * (self.height - 100), 100, 100);
+			self.ctx.fillRect(Math.random() * (self.width - 100), Math.random() * (self.height - 100), 100, 100);
+		}
+	};
+
+	this.run = function (frameTime) {
+		if (self.running) {
+			var tickCount = Math.floor((frameTime - self.updateTime) / self.tickLength), i;
+	        if (tickCount > 0) {
+	            /*logic(tickCount);
+	            render();*/
+
+				self.updateTime += tickCount * self.tickLength;
+
+				self.tickCount += tickCount;
+
+				if(self.tickCount > self.tickMax) {
+					self.tickCount = 0;
+				}
+
+				for (i = 0; i < self.buttons.length; i += 1) {
+					self.buttons[i].redrawBackground(self.tickCount);
+				}
+
+				self.redrawButtons();
+	        }
+			window.requestAnimationFrame(self.run);
+		}
+	};
 }
 
 Menu.prototype.init = function () {
 	this.canvas.addEventListener('mouseup', this.listener_mouseup);
 	this.canvas.addEventListener('mousedown', this.listener_mousedown);
 	this.canvas.addEventListener('mousemove', this.listener_mousemove);
-	this.redrawMenu();
-}
+	this.redrawButtons();
+	this.running = true;
+
+	if (this.animated) {
+		this.updateTime = window.performance.now();
+		window.requestAnimationFrame(this.run);
+	}
+};
 
 Menu.prototype.destroy = function () {
 	var i;
-	
+
 	this.canvas.removeEventListener('mouseup', this.listener_mouseup);
 	this.canvas.removeEventListener('mousedown', this.listener_mousedown);
 	this.canvas.removeEventListener('mousemove', this.listener_mousemove);
-	
+
 	for (i = 0; i < this.buttons.length; i += 1) {
 		this.buttons[i].setState(BUTTON_ENUM.inactive);
-		this.redrawMenu();
+		this.redrawButtons();
 	}
-}
+
+	this.running = false;
+};
 
 Menu.prototype.appendButton = function (button) {
 	this.buttons.push(button);
 	button.setParentMenu(this);
-}
+};
 
 var BUTTON_ENUM = {
 	"inactive": 0,
 	"focused": 1,
 	"down": 2,
 	"up": 3
-}
+};
 
-function Button(x, y, width, height, text) {
+function Button(x, y, width, height, text, redrawInactive, redrawFocused, redrawDown, redrawUp) {
 	this.x = x;
 	this.y = y;
 	this.width = width;
@@ -103,25 +169,24 @@ function Button(x, y, width, height, text) {
 	this.x_limit = this.x + this.width;
 	this.y_limit = this.y + this.height;
 	this.state = BUTTON_ENUM.inactive;
-	this.tick = 0;
-	this.maxTick = 60;
-	
 	this.canvas_inactive = document.createElement('canvas');
 	this.canvas_inactive.width = width;
 	this.canvas_inactive.height = height;
-	
+	this.tick = 0;
+	this.text = text;
+
 	this.canvas_focused = document.createElement('canvas');
 	this.canvas_focused.width = width;
 	this.canvas_focused.height = height;
-	
+
 	this.canvas_down = document.createElement('canvas');
 	this.canvas_down.width = width;
 	this.canvas_down.height = height;
-	
+
 	this.canvas_up = document.createElement('canvas');
 	this.canvas_up.width = width;
 	this.canvas_up.height = height;
-	
+
 	this.redraw = function () {
 		if (this.state === BUTTON_ENUM.inactive) {
 			this.redrawInactive();
@@ -132,22 +197,22 @@ function Button(x, y, width, height, text) {
 		} else if (this.state === BUTTON_ENUM.up) {
 			this.redrawUp();
 		}
-	}
-	
-	this.redrawInactive = function () {
+	};
+
+	this.redrawInactive = redrawInactive || function () {
 		var ctx = this.canvas_inactive.getContext('2d');
 		ctx.clearRect(0, 0, this.width, this.height);
-		ctx.fillStyle = "green";
+		ctx.fillStyle = 'rgb(' + (100 - this.tick) + ', 0, 0)';
 		ctx.fillRect(0, 0, this.width, this.height);
 		ctx.font = '30pt Arial';
 		ctx.textAlign="center";
 		ctx.fillStyle = "white";
 		ctx.fillText(text, this.width / 2, (this.height - 30) / 2 + 30);
-	}
-	
+	};
+
 	this.redrawInactive();
-	
-	this.redrawFocused = function () {
+
+	this.redrawFocused = redrawFocused || function () {
 		var ctx = this.canvas_focused.getContext('2d');
 		ctx.clearRect(0, 0, this.width, this.height);
 		ctx.fillStyle = "blue";
@@ -156,11 +221,11 @@ function Button(x, y, width, height, text) {
 		ctx.textAlign="center";
 		ctx.fillStyle = "white";
 		ctx.fillText(text, this.width / 2, (this.height - 30) / 2 + 30);
-	}
-	
+	};
+
 	this.redrawFocused();
-	
-	this.redrawDown = function () {
+
+	this.redrawDown = redrawDown || function () {
 		var ctx = this.canvas_down.getContext('2d');
 		ctx.clearRect(0, 0, this.width, this.height);
 		ctx.fillStyle = "red";
@@ -169,11 +234,11 @@ function Button(x, y, width, height, text) {
 		ctx.textAlign="center";
 		ctx.fillStyle = "white";
 		ctx.fillText(text, this.width / 2, (this.height - 30) / 2 + 30);
-	}
-	
+	};
+
 	this.redrawDown();
-	
-	this.redrawUp = function () {
+
+	this.redrawUp = redrawUp || function () {
 		var ctx = this.canvas_up.getContext('2d');
 		ctx.clearRect(0, 0, this.width, this.height);
 		ctx.fillStyle = "orange";
@@ -182,8 +247,8 @@ function Button(x, y, width, height, text) {
 		ctx.textAlign="center";
 		ctx.fillStyle = "black";
 		ctx.fillText(text, this.width / 2, (this.height - 30) / 2 + 30);
-	}
-	
+	};
+
 	this.redrawUp();
 }
 
@@ -192,22 +257,17 @@ Button.prototype.inRange = function (x, y) {
 		return true;
 	}
 	return false;
-}
+};
 
 Button.prototype.setState = function (newState) {
 	this.state = newState;
 	this.tick = 0;
-}
+};
 
 Button.prototype.redrawBackground = function (step) {
-	this.tick += step;
-	
-	if (this.tick > 60) {
-		this.tick = step - 60;
-	}
-	
-	this.redraw
-}
+	this.tick = step;
+	this.redraw();
+};
 
 Button.prototype.getCanvas = function () {
 	if (this.state === BUTTON_ENUM.inactive) {
@@ -219,23 +279,23 @@ Button.prototype.getCanvas = function () {
 	} else if (this.state === BUTTON_ENUM.up) {
 		return this.canvas_up;
 	}
-}
+};
 
 Button.prototype.getX = function () {
 	return this.x;
-}
+};
 
 Button.prototype.getY = function () {
 	return this.y;
-}
+};
 
 Button.prototype.setParentMenu = function (menu) {
 	this.menu = menu;
-}
+};
 
 Button.prototype.clickHandler = function (handler) {
 	this.onclick = handler;
-}
+};
 
 Button.prototype.click = function () {
 	if (this.onclick instanceof Menu) {
@@ -246,4 +306,4 @@ Button.prototype.click = function () {
 			this.onclick.call();
 		}
 	}
-}
+};
--- test/config.js
+++ test/config.js
@@ -3,12 +3,21 @@ var width = 800,
 	buttonWidth = 300,
 	buttonHegith = 50;
 
-var mainMenu = new Menu(document.getElementById('canvas'), width, height);
-var newGameMenu = new Menu(document.getElementById('canvas'), width, height);
-var optionsMenu = new Menu(document.getElementById('canvas'), width, height);
-var aboutMenu = new Menu(document.getElementById('canvas'), width, height);
+var mainMenu = new Menu(document.getElementById('canvas'), width, height, true);
+var newGameMenu = new Menu(document.getElementById('canvas'), width, height, false);
+var optionsMenu = new Menu(document.getElementById('canvas'), width, height, false);
+var aboutMenu = new Menu(document.getElementById('canvas'), width, height, false);
 
-var newGameMainMenuButton = new Button(width / 2 - buttonWidth / 2, 300, buttonWidth, buttonHegith, "New Game");
+var newGameMainMenuButton = new Button(width / 2 - buttonWidth / 2, 300, buttonWidth, buttonHegith, "New Game", function () {
+	var ctx = this.canvas_inactive.getContext('2d');
+	ctx.clearRect(0, 0, this.width, this.height);
+	ctx.fillStyle = 'rgb(' + (100 - this.tick) + ', 0, 0)';
+	ctx.fillRect(0, 0, this.width, this.height);
+	ctx.font = '30pt Arial';
+	ctx.textAlign="center";
+	ctx.fillStyle = "white";
+	ctx.fillText(this.text, this.width / 2, (this.height - 30) / 2 + 30);
+});
 newGameMainMenuButton.clickHandler(newGameMenu);
 
 var optionsMainMenuButton = new Button(width / 2 - buttonWidth / 2, 300 + buttonHegith + 20, buttonWidth, buttonHegith, "Options");
