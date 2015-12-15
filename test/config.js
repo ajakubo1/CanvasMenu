3fcd7d80b7bcac55cc29f09624f9c972713450d3
Adam Jakubowski <ajakubo1@gmail.com>
2015-12-15 23:58:52 +0100

    corrected misspell

--- test/config.js
+++ test/config.js
@@ -1,7 +1,7 @@
 var width = 800,
 	height = 600,
 	buttonWidth = 300,
-	buttonHegith = 50;
+	buttonHeight = 50;
 
 
 
@@ -9,10 +9,12 @@ var width = 800,
 var squares = [];
 
 var menuAnimation = function () {
+    var i;
+
     if (this.tickCount % 5 === 0 && squares.length !== 8 * 6) {
         squares.push(10);
     } else if (this.tickCount % 5 === 0) {
-        for (var i = 0; i < squares.length ; i += 1) {
+        for (i = 0; i < squares.length ; i += 1) {
             if(squares[i] === -101) {
                 squares[i] = 10;
                 break;
@@ -21,7 +23,7 @@ var menuAnimation = function () {
     }
 
     this.ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (var i = 0; i < squares.length ; i += 1) {
+    for (i = 0; i < squares.length ; i += 1) {
         if(squares[i] > -100) {
             this.ctx.fillStyle = 'rgb(' + (100 + squares[i]) + ', 0, 0)';
             this.ctx.fillRect(i % 8 * 100, Math.floor(i / 8) * 100, 100, 100);
@@ -30,7 +32,7 @@ var menuAnimation = function () {
     }
 
     if (squares.length === 8 * 6 && squares[squares.length - 1] === 8) {
-        for (var i = 0; i < squares.length ; i += 1) {
+        for (i = 0; i < squares.length ; i += 1) {
             if(squares[i] === -100) {
                 squares[i] = -101;
             }
@@ -38,7 +40,7 @@ var menuAnimation = function () {
     }
 
     if (squares.length === 8 * 6 && squares[squares.length - 1] === -100) {
-        for (var i = squares.length - 1; i >= 0 ; i -= 1) {
+        for (i = squares.length - 1; i >= 0 ; i -= 1) {
             if(squares[i] === -100) {
                 squares[i] = -101;
             } else {
@@ -53,28 +55,28 @@ var newGameMenu = new Menu(document.getElementById('canvas'), width, height, fal
 var optionsMenu = new Menu(document.getElementById('canvas'), width, height, false);
 var aboutMenu = new Menu(document.getElementById('canvas'), width, height, false);
 
-var newGameMainMenuButton = new Button(width / 2 - buttonWidth / 2, 270, buttonWidth, buttonHegith, "New Game");
+var newGameMainMenuButton = new Button(width / 2 - buttonWidth / 2, 270, buttonWidth, buttonHeight, "New Game");
 newGameMainMenuButton.clickHandler(newGameMenu);
 
-var optionsMainMenuButton = new Button(width / 2 - buttonWidth / 2, 270 + buttonHegith + 20, buttonWidth, buttonHegith, "Options");
+var optionsMainMenuButton = new Button(width / 2 - buttonWidth / 2, 270 + buttonHeight + 20, buttonWidth, buttonHeight, "Options");
 optionsMainMenuButton.clickHandler(optionsMenu);
 
-var aboutMainMenuButton = new Button(width / 2 - buttonWidth / 2, 270 + 2 * (buttonHegith + 20), buttonWidth, buttonHegith, "About");
+var aboutMainMenuButton = new Button(width / 2 - buttonWidth / 2, 270 + 2 * (buttonHeight + 20), buttonWidth, buttonHeight, "About");
 aboutMainMenuButton.clickHandler(aboutMenu);
 
 mainMenu.appendButton(newGameMainMenuButton);
 mainMenu.appendButton(optionsMainMenuButton);
 mainMenu.appendButton(aboutMainMenuButton);
 
-var backOptionsButton = new Button(10, height - 60, buttonHegith, buttonHegith, String.fromCharCode(parseInt("21DC", 16)));
+var backOptionsButton = new Button(10, height - 60, buttonHeight, buttonHeight, String.fromCharCode(parseInt("21DC", 16)));
 backOptionsButton.clickHandler(mainMenu);
 newGameMenu.appendButton(backOptionsButton);
 
-var backNewGameButton = new Button(10, height - 60, buttonHegith, buttonHegith, String.fromCharCode(parseInt("21DC", 16)));
+var backNewGameButton = new Button(10, height - 60, buttonHeight, buttonHeight, String.fromCharCode(parseInt("21DC", 16)));
 backNewGameButton.clickHandler(mainMenu);
 optionsMenu.appendButton(backNewGameButton);
 
-var backAboutButton = new Button(10, height - 60, buttonHegith, buttonHegith, String.fromCharCode(parseInt("21DC", 16)));
+var backAboutButton = new Button(10, height - 60, buttonHeight, buttonHeight, String.fromCharCode(parseInt("21DC", 16)));
 backAboutButton.clickHandler(mainMenu);
 aboutMenu.appendButton(backAboutButton);
 
