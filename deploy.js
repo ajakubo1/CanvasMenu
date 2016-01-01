db885109aa62745122a17578782aaae86ad13f44
Adam Jakubowski <ajakubo1@gmail.com>
2016-01-01 21:53:02 +0100

    Changed deploy script to be more idiot-proof, added build destination

--- deploy.js
+++ deploy.js
@@ -1,38 +1,114 @@
 #! /usr/bin/env node
 
 var uglify = require('uglify-js'),
-    fs = require('fs');
+    fs = require('fs'),
+    path = require('path');
 
-var arguments = Array.prototype.slice.call(process.argv, 2),
-    i, prefix = "src/", postfix = ".js", uglified;
+var args = [], prefix = "src/", postfix = ".js", destination = "", uglified, go = true;
 
-for (i = 0 ; i < arguments.length; i += 1) {
-    arguments[i] = prefix + arguments[i] + postfix;
 
-    var content = fs.readFileSync(arguments[i]);
-    if(i === 0) {
-        fs.writeFileSync('dist/canvasmenu.js', content);
-    } else {
-        fs.appendFileSync('dist/canvasmenu.js', content);
+function main() {
+    var i;
+
+    if (fs.existsSync(path.normalize(destination + 'canvasmenu.js'))) {
+        fs.unlinkSync(path.normalize(destination + 'canvasmenu.js'));
+    }
+
+    if (fs.existsSync(path.normalize(destination + 'canvasmenu.map'))) {
+        fs.unlinkSync(path.normalize(destination + 'canvasmenu.map'));
+    }
+
+    if (fs.existsSync(path.normalize(destination + 'canvasmenu.min.js'))) {
+        fs.unlinkSync(path.normalize(destination + 'canvasmenu.min.js'));
+    }
+
+    fs.writeFileSync(path.normalize(destination + 'canvasmenu.js'), "");
+
+    for (i = 0 ; i < args.length; i += 1) {
+        fs.appendFileSync(path.normalize(destination + 'canvasmenu.js'), fs.readFileSync(args[i]));
+    }
+
+    uglified = uglify.minify(args, {
+        "outSourceMap": path.normalize(destination + 'canvasmenu.map')
+    });
+
+    fs.writeFile(path.normalize(destination + 'canvasmenu.min.js'), uglified.code, function (err){
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("Minified script generated and saved:", 'dist/canvasmenu.min.js');
+        }
+    });
+
+    fs.writeFile(path.normalize(destination + 'canvasmenu.map'), uglified.map, function (err){
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("Map generated and saved:", path.normalize(destination + 'canvasmenu.map'));
+        }
+    });
+}
+
+function help() {
+    console.info('Applicable args:');
+    console.info('\t--help - print help message');
+    console.info('\t--dest=<deployment destination> - change destination for script building');
+    console.info('\nArguments without -- sign will be considered as element names')
+}
+
+function applyArgument (arg) {
+    switch(arg.split('=')[0]) {
+        case '--dest':
+            destination = arg.replace('--dest=', '');
+            break;
+        case '--help':
+            help();
+            go = false;
+            break;
+        default:
+            console.warn('This script does not understand: ' + arg);
+            help();
+            go = false;
     }
 }
 
-var uglified = uglify.minify(arguments, {
-    "outSourceMap": 'dist/canvasmenu.map'
-});
-
-fs.writeFile('dist/canvasmenu.min.js', uglified.code, function (err){
-    if(err) {
-        console.log(err);
-    } else {
-        console.log("Minified script generated and saved:", 'dist/canvasmenu.min.js');
+function removeFalsy() {
+    var i;
+    for (i = 0 ; i < args.length; i += 1) {
+        if (args[i] === false) {
+            args.splice(i, 1);
+            break;
+        }
     }
-});
 
-fs.writeFile('dist/canvasmenu.map', uglified.map, function (err){
-    if(err) {
-        console.log(err);
-    } else {
-        console.log("Map generated and saved:", 'dist/canvasmenu.map');
+    if(i !== args.length) {
+        removeFalsy();
     }
-});
+}
+
+function extractArguments() {
+    var i, temp = process.argv.slice(2);
+    for (i = 0; i < temp.length; i += 1) {
+        if (temp[i].startsWith('--')) {
+            applyArgument(temp[i]);
+        } else {
+            args.push(temp[i]);
+        }
+    }
+
+    for (i = 0 ; i < args.length; i += 1) {
+        args[i] = path.normalize(prefix + args[i] + postfix);
+
+        if (!fs.existsSync(args[i])) {
+            console.warn("There is no file called: " + args[i] + " - it will be ignored during library build");
+            args[i] = false;
+        }
+    }
+
+    removeFalsy();
+}
+
+extractArguments();
+if (go) {
+    main();
+}
